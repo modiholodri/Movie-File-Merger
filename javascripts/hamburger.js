af51bbe2262c71549b8b371d8e6d4cd0e7014e6c
Modi777 <reinhold.lauer@gmail.com>
2014-12-07 19:22:57 +0700

    Changed to Hamburger menu

---
+++ javascripts/hamburger.js
@@ -0,0 +1,61 @@
+/**
+ * hamburger.js
+ *
+ * Mobile Menu Hamburger
+ * =====================
+ * A hamburger menu for mobile websites
+ *
+ * Created by Thomas Zinnbauer YMC AG  |  http://www.ymc.ch
+ * Date: 21.05.13
+ */
+
+jQuery(document).ready(function () {
+
+    //Open the menu
+    jQuery("#hamburger").click(function () {
+
+        jQuery('#content').css('min-height', jQuery(window).height());
+
+        jQuery('nav').css('opacity', 1);
+
+        //set the width of primary content container -> content should not scale while animating
+        var contentWidth = jQuery('#content').width();
+
+        //set the content with the width that it has originally
+        jQuery('#content').css('width', contentWidth);
+
+        //display a layer to disable clicking and scrolling on the content while menu is shown
+        jQuery('#contentLayer').css('display', 'block');
+
+        //disable all scrolling on mobile devices while menu is shown
+        jQuery('#container').bind('touchmove', function (e) {
+            e.preventDefault()
+        });
+
+        //set margin for the whole container with a jquery UI animation
+        jQuery("#container").animate({"marginLeft": ["100px", 'easeOutExpo']}, {
+            duration: 2000
+        });
+
+    });
+
+    //close the menu
+    jQuery("#contentLayer").click(function () {
+
+        //enable all scrolling on mobile devices when menu is closed
+        jQuery('#container').unbind('touchmove');
+
+        //set margin for the whole container back to original state with a jquery UI animation
+        jQuery("#container").animate({"marginLeft": ["-1", 'easeOutExpo']}, {
+            duration: 2000,
+            complete: function () {
+                jQuery('#content').css('width', 'auto');
+                jQuery('#contentLayer').css('display', 'none');
+                jQuery('nav').css('opacity', 0);
+                jQuery('#content').css('min-height', 'auto');
+
+            }
+        });
+    });
+
+});
