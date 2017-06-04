e42ec96c8c7f5c64820911d355cf9176e82323c9
Modi777 <reinhold.lauer@gmail.com>
2017-06-04 11:36:57 +0700

    Prepared MFM 0.6.0.

--- Movie File Merger/Properties/AssemblyInfo.cs
+++ Movie File Merger/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Movie File Merger")]
-[assembly: AssemblyDescription("Selectively Combine/Maintain Collections on Hard Disks")]
+[assembly: AssemblyDescription("Selectively Combine/Maintain Video Collections")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
